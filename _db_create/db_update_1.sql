afc52e7f785aede39ef3c10abbbaa759509a246b
erilya <ilja_eremin@mail.ru>
2019-05-24 21:20:06 +0300

    Исправлен комментарий к столбцу sql-скрипта

--- _db_create/db_update_1.sql
+++ _db_create/db_update_1.sql
@@ -3,7 +3,7 @@ CREATE TABLE `admin_user` (
   `admin_user_surname` varchar(45) NOT NULL COMMENT 'Фамилия секретаря',
   `admin_user_firstname` varchar(45) NOT NULL COMMENT 'Имя секретаря',
   `admin_user_middlename` varchar(45) DEFAULT NULL COMMENT 'Отчество секретаря',
-  `admin_user_login` varchar(45) DEFAULT NULL COMMENT 'Отчество секретаря',
+  `admin_user_login` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`admin_user_id`),
   UNIQUE KEY `admin_user_login_UNIQUE` (`admin_user_login`)
 ) COMMENT='Административный пользователь (секретарь)';
