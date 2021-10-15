5baa33a9cf3f306d9a7b823549b18296a6e0f71a
Vitya535 <kushnerenko.viktor@gmail.com>
2021-10-15 12:51:12 +0300

    Поправил некоторые сообщения (написал на русском)

--- templates/js/common_operations_for_entities.js
+++ templates/js/common_operations_for_entities.js
@@ -7,8 +7,8 @@ $(function () {
 
     $('#table_with_records').DataTable({
         "language": {
-            "info": "Showing _START_.._END_ of _TOTAL_ entries",
-            "lengthMenu": "Show Items per Page: _MENU_",
+            "info": "Показ _START_.._END_ из _TOTAL_ записей",
+            "lengthMenu": "Записей на странице: _MENU_",
             "paginate": {
                 "first": "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"24\" height=\"24\" fill=\"currentColor\" class=\"bi bi-skip-start-fill\" viewBox=\"0 0 16 16\">\n" +
                     "<path d=\"M4 4a.5.5 0 0 1 1 0v3.248l6.267-3.636c.54-.313 1.232.066 1.232.696v7.384c0 .63-.692 1.01-1.232.697L5 8.753V12a.5.5 0 0 1-1 0V4z\" />\n" +
